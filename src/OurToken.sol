2edce0300c0476bbcbd4f2f75ac4b7b0939b27e4
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2024-01-01 16:32:01 +0100

    added our token contract

---
+++ src/OurToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// -=-<[ Bismillahirrahmanirrahim ]>-=-
+// -*- coding: utf-8 -*-
+// @Date    : 2024-01-01 16:25:56
+// @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
+pragma solidity 0.8.19;
+
+contract OurToken {
+    // code here
+}
+
