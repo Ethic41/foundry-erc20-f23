542fe501af241ff6e00ef56a1c08410a6937a2f6
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2024-01-01 16:59:25 +0100

    updated OurToken contract

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -3,9 +3,13 @@
 // -*- coding: utf-8 -*-
 // @Date    : 2024-01-01 16:25:56
 // @Author  : Dahir Muhammad Dahir (dahirmuhammad3@gmail.com)
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
-contract OurToken {
-    // code here
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OurToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
+    }
 }
 
